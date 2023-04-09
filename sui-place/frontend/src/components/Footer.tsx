6ce01e511946365a254cc97075aaa72ac089abe1
watercrypto <stayhydrated.crypto@gmail.com>
2023-04-09 17:14:39 +0800

    Added footer links

--- sui-place/frontend/src/components/Footer.tsx
+++ sui-place/frontend/src/components/Footer.tsx
@@ -1,14 +1,24 @@
 import { TiSocialTwitter } from "react-icons/ti";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <footer className="rounded-md w-full flex">
-      <a
-        href="https://twitter.com/watercrypt0"
-        className="flex items-center text-xs mx-auto gap-[2px] opacity-40 hover:opacity-70"
-      >
-        <TiSocialTwitter size={14} /> watercrypt0
-      </a>
+    <footer className="rounded-md w-full flex text-xs items-center">
+      <span className="opacity-40">Made by @watercrypt0</span>
+      <span className="ml-auto flex items-center gap-[2px]">
+        <a
+          href="https://twitter.com/watercrypt0"
+          className="flex opacity-40 hover:opacity-70"
+        >
+          <TiSocialTwitter size={18} />
+        </a>
+        <a
+          href="https://github.com/watercrypto/sui-projects/tree/main/sui-place"
+          className="flex opacity-40 hover:opacity-70"
+        >
+          <AiFillGithub size={14} />
+        </a>
+      </span>
     </footer>
   );
 }
